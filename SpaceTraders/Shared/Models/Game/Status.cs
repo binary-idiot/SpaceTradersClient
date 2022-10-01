eafa82fd2c1c0f5d19f109a581a9c2be25494ad1
Jonah Middleton <jonah.middleton@gmail.com>
2022-09-30 23:07:33 -0400

    Create Status component

---
+++ SpaceTraders/Shared/Models/Game/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceTraders.Shared.Models.Game;
+
+public class GameStatus
+{
+	public string Status { get; set; }
+
+	public bool IsOnline
+	{
+		get => Status == "spacetraders is currently online and available to play";
+	}
+}
---
+++ SpaceTraders/Shared/Services/Data/GameStatusService.cs
@@ -0,0 +1,24 @@
+﻿using SpaceTraders.Shared.Models.Game;
+using SpaceTraders.Shared.Services.API;
+
+namespace SpaceTraders.Shared.Services.Data;
+
+public class GameStatusService : IDataService
+{
+	private readonly GameApiService _api;
+
+	public GameStatusService(GameApiService api)
+	{
+		_api = api;
+	}
+
+	public async Task<GameStatus> GetStatus()
+	{
+		ApiResponse<GameStatus> response = await _api.Get<GameStatus>(new ApiQuery()
+		{
+			Endpoint = "game/status"
+		});
+
+		return response.Result;
+	}
+}
