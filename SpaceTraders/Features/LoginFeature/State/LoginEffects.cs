66244d766ed108e1f57b1763e64dd8dc61355743
Jonah Middleton <jonah.middleton@gmail.com>
2022-10-09 23:38:48 -0400

    Add LoginEffects

---
+++ SpaceTraders/Features/LoginFeature/State/LoginEffects.cs
@@ -0,0 +1,49 @@
+﻿using Fluxor;
+
+namespace SpaceTraders.Features.LoginFeature.State;
+
+public class LoginEffects
+{
+	private readonly IServiceScopeFactory _serviceScopeFactory;
+
+	public LoginEffects(IServiceScopeFactory serviceScopeFactory)
+	{
+		_serviceScopeFactory = serviceScopeFactory;
+	}
+
+	[EffectMethod(typeof(LoadLoginAction))]
+	public async Task HandleLoadLoginAction(IDispatcher dispatcher)
+	{
+		using (IServiceScope scope = _serviceScopeFactory.CreateScope())
+		{
+			LoginService? loginService = scope.ServiceProvider.GetService<LoginService>();
+			if (loginService is not null)
+			{
+				Login login = await loginService.GetSavedLogin();
+				dispatcher.Dispatch(new LoadLoginSuccessAction(login));
+			}
+			else
+			{
+				dispatcher.Dispatch(new LoadLoginFailureAction("LoginService is null"));
+			}
+		}
+	}
+	
+	[EffectMethod]
+	public async Task HandleLoginAction(LoginAction action, IDispatcher dispatcher)
+	{
+		using (IServiceScope scope = _serviceScopeFactory.CreateScope())
+		{
+			LoginService loginService = scope.ServiceProvider.GetService<LoginService>();
+			if (loginService is not null)
+			{
+				await loginService.SetSavedLogin(action.Login);
+				dispatcher.Dispatch(new LoginSuccessAction(action.Login));
+			}
+			else
+			{
+				dispatcher.Dispatch(new LoginFailureAction("LoginService is null"));
+			}
+		}
+	}
+}
