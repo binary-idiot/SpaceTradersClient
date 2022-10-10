e3ee8dfd57acc1b777fe55c861ef9b3655cebc69
Jonah Middleton <jonah.middleton@gmail.com>
2022-10-09 23:38:13 -0400

    Add logging to LoginService

--- SpaceTraders/Features/LoginFeature/LoginService.cs
+++ SpaceTraders/Features/LoginFeature/LoginService.cs
@@ -7,19 +7,31 @@ public class LoginService : IDataService
 {
 	private readonly string key = "login";
 	private readonly ILocalStorageService _localStorage;
+	private readonly ILogger _logger;
 
-	public LoginService(ILocalStorageService localStorage)
+	public LoginService(ILocalStorageService localStorage, ILogger<LoginService> logger)
 	{
 		_localStorage = localStorage;
+		_logger = logger;
 	}
 	
 	public async Task<Login> GetSavedLogin()
 	{
-		return await _localStorage.GetItemAsync<Login>(key);
+		Login login = await _localStorage.GetItemAsync<Login>(key) ?? new Login();
+		if (string.IsNullOrWhiteSpace(login?.Username) && string.IsNullOrWhiteSpace(login?.Token))
+		{
+			_logger.LogInformation($"No saved login found");
+		}
+		else
+		{
+			_logger.LogInformation($"Loaded login (Username: {login.Username}, Token: {login.Token})");
+		}
+		return login;
 	}
 
 	public async Task SetSavedLogin(Login login)
 	{
+		_logger.LogInformation($"Saving login (Username: {login.Username}, Token: {login.Token})");
 		await _localStorage.SetItemAsync(key, login);
 	}
 }
