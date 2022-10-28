7088cd19c07a93f6180de206f3019dfa64003fda
Jonah Middleton <jonah.middleton@gmail.com>
2022-10-28 19:40:51 -0400

    Modify ServiceCollectionExtensions to register with inteface as serviceType

--- SpaceTraders/Shared/Utilities/ServiceCollectionExtensions.cs
+++ SpaceTraders/Shared/Utilities/ServiceCollectionExtensions.cs
@@ -2,34 +2,44 @@
 
 public static class ServiceCollectionExtensions
 {
-	public static IServiceCollection AddTransientServicesWithInterface<TInterface>(this IServiceCollection services)
+	public static IServiceCollection AddTransientServicesWithInterface<TInterface>(this IServiceCollection services, bool useInterfaceForServiceType = false)
 	{
-		IEnumerable<Type> typesToRegister = DiscoverTypes<TInterface>();
-
-		foreach (var type in typesToRegister)
-		{
-			services.AddTransient(type, type);
-		}
-
+		AddWithInterface<TInterface>(services.AddTransient, useInterfaceForServiceType);
 		return services;
 	}
 	
-	public static IServiceCollection AddSingletonServicesWithInterface<TInterface>(this IServiceCollection services)
+	public static IServiceCollection AddSingletonServicesWithInterface<TInterface>(this IServiceCollection services, bool useInterfaceForServiceType = false)
+	{
+		AddWithInterface<TInterface>(services.AddSingleton, useInterfaceForServiceType);
+		return services;
+	}
+
+	private static void AddWithInterface<TInterface>(Func<Type, Type, IServiceCollection> addMethod, bool useInterfaceForServiceType)
 	{
 		IEnumerable<Type> typesToRegister = DiscoverTypes<TInterface>();
 
-		foreach (var type in typesToRegister)
+		foreach (Type type in typesToRegister)
 		{
-			services.AddSingleton(type, type);
+			Type serviceType = type;
+			if (useInterfaceForServiceType)
+			{
+				serviceType = type.GetSpecificInterface(typeof(TInterface)) ?? serviceType;
+			}
+			
+			addMethod.Invoke(serviceType! , type!);
 		}
-
-		return services;
-	} 
-
+	}
 	private static IEnumerable<Type> DiscoverTypes<TInterface>()
 	{
 		return typeof(TInterface).Assembly
 			.GetTypes()
 			.Where(t => t.IsClass && t.IsAssignableTo(typeof(TInterface)) && !t.IsAbstract);
+	}
+
+	private static Type? GetSpecificInterface(this Type type, Type interfaceType)
+	{
+		Type[] interfaces = type.GetInterfaces();
+		interfaces = interfaces.Where((Type i) => i.IsAssignableTo(interfaceType) && i.IsConstructedGenericType).ToArray();
+		return interfaces.FirstOrDefault();
 	}
 }
