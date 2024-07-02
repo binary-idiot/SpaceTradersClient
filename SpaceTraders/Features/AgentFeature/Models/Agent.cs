7a0c1f1b6d8da1ce7fb1e87c5ec19ea6e721a31e
Jonah Middleton <jonah.middleton@gmail.com>
2024-07-02 01:50:19 -0400

    Create Location model

--- SpaceTraders/Features/AgentFeature/Models/Agent.cs
+++ SpaceTraders/Features/AgentFeature/Models/Agent.cs
@@ -9,7 +9,7 @@ public class Agent
 {
 	public string AccountId { get; set; }
 	public string Symbol { get; set; }
-	public string Headquarters { get; set; }
+	public Location Headquarters { get; set; }
 	public int Credits { get; set; }
 	public string StartingFaction { get; set; }
 	public int ShipCount { get; set; }
---
+++ SpaceTraders/Shared/Models/Game/Location.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SpaceTraders.Shared.Models.Game;
+
+[JsonConverter(typeof(LocationJsonConverter))]
+public class Location
+{
+	public string Sector { get; }
+	public string System { get; }
+	public string Waypoint { get; }
+
+	public string FullSystem
+	{
+		get => $"{Sector}{PrefixPart(System)}";
+	}
+	
+	public string FullWaypoint
+	{
+		get => $"{FullSystem}{PrefixPart(Waypoint)}";
+	}
+
+	public Location(string? location)
+	{
+		location ??= string.Empty;
+		string[] parts = location.Split('-');
+		Sector = (parts.Length > 0) ? parts[0] : String.Empty;
+		System = (parts.Length > 1) ? parts[1] : String.Empty;
+		Waypoint = (parts.Length > 2) ? parts[2] : String.Empty;
+	}
+
+	public static implicit operator string(Location l) => l.FullWaypoint;
+	public static implicit operator Location(string l) => new Location(l);
+	public override string ToString() => FullWaypoint;
+
+	private static string PrefixPart(string part)
+	{
+		if (string.IsNullOrWhiteSpace(part))
+		{
+			return String.Empty;
+		}
+
+		return $"-{part}";
+	}
+}
+
+public class LocationJsonConverter : JsonConverter<Location>
+{
+	public override Location? Read(
+		ref Utf8JsonReader reader,
+		Type typeToConvert,
+		JsonSerializerOptions options) => new Location(reader.GetString());
+
+	public override void Write(
+		Utf8JsonWriter writer,
+		Location value,
+		JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
+}
